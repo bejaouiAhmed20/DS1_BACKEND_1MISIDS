a926d775f6e7c8f3472351acdc844691c6e56a20
Oumaima Ben Ammar <oumaimabenammar67@gmail.com>
2025-12-05 19:34:45 +0100

    comments for db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,19 @@
+// 3malna Importation de la bibliothèque Mongoose
 const mongoose = require("mongoose");
 
+// Fonction asynchrone pour établir la connexion à la base de données MongoDB
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+
+    // Message de confirmation si la connexion réussit
     console.log("MongoDB connected");
   } catch (err) {
+
+    // Affiche l'erreur en cas d'échec de la connexion
     console.error(err);
+
+    // On va arreter complètement le processus avec un code d'erreur
     process.exit(1);
   }
 };
